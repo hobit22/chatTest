1783e2150b0f4b443669c380daa547419c6398c7
hobit22 <hobit22@naver.com>
2022-04-27 21:45:11 +0900

    Initial commit

---
+++ src/main/java/com/chatting/ver1/Ver1Application.java
@@ -0,0 +1,13 @@
+package com.chatting.ver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ver1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ver1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chatting/ver1/Ver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatting.ver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ver1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
